face7e669a6c5c051cbc837f7784a5ece0b4f4f2
23jrg <jrg71442@yahoo.com>
2025-11-21 14:09:08 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,1 +1,15 @@
-invoke-expression 'cmd /c start powershell -Command {irm https://get.activated.win | iex}';invoke-expression 'cmd /c start powershell -Command {irm "https://christitus.com/win" | iex}';winget install ZhornSoftware.Caffeine --source winget --force;;$env:Path = [System.Environment]::GetEnvironmentVariable("Path","User");$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine");Caffeine -activefor:15 -replace;git clone https://github.com/23jrg/Kick-Inactive-Users;.\Kick-Inactive-Users\setup.bat;git clone https://github.com/lzw29107/MediaCreationTool.bat;.\MediaCreationTool.bat\MediaCreationTool.bat
+#Guibased Tools
+invoke-expression 'cmd /c start powershell -Command {irm https://get.activated.win | iex}';
+invoke-expression 'cmd /c start powershell -Command {irm "https://christitus.com/win" | iex}';
+
+#installs a program that keeps the computer from sleeping
+winget install ZhornSoftware.Caffeine --source winget --force;
+
+#Refreshes the powershell path to use all the cool stuff we just added to it
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
+#Keeps computer awake while you're working on it
+;Caffeine -activefor:15 -replace;git clone https://github.com/23jrg/Kick-Inactive-Users;.\Kick-Inactive-Users\setup.bat;
+
+#Handy Windows updater
+git clone https://github.com/lzw29107/MediaCreationTool.bat;.\MediaCreationTool.bat\MediaCreationTool.bat
