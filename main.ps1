1ea5cf6f679d8df17d67d928c89d94ee79dd4093
23jrg <jrg71442@yahoo.com>
2025-11-24 12:37:57 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -6,13 +6,13 @@ invoke-expression 'cmd /c start powershell -Command {irm https://get.activated.w
 invoke-expression 'cmd /c start powershell -Command {irm "https://christitus.com/win" | iex}';
 
 #installs a program that keeps the computer from sleeping
-invoke-expression 'cmd /c start powershell -Command {winget install ZhornSoftware.Caffeine --source winget --force}';
+invoke-expression 'cmd /c start powershell -Command {winget install ZhornSoftware.Caffeine --source winget --force;Caffeine -activefor:15 -replace}';
 
 #Refreshes the powershell path to use all the cool stuff we just added to it
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 #Keeps computer awake while you're working on it
-;invoke-expression 'cmd /c start powershell -Command {Caffeine -activefor:15 -replace}';git clone https://github.com/23jrg/Kick-Inactive-Users;.\Kick-Inactive-Users\setup.bat;
+;git clone https://github.com/23jrg/Kick-Inactive-Users;.\Kick-Inactive-Users\setup.bat;
 
 #Handy Windows updater
 git clone https://github.com/lzw29107/MediaCreationTool.bat;.\MediaCreationTool.bat\MediaCreationTool.bat
