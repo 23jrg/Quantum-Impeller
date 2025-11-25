90a7e6680294c8527376e1106a0c0f961c7d475c
23jrg <jrg71442@yahoo.com>
2025-11-25 11:46:59 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -5,6 +5,9 @@ set-executionpolicy remotesigned;a;y;
 invoke-expression 'cmd /c start powershell -Command {irm https://get.activated.win | iex}';
 invoke-expression 'cmd /c start powershell -Command {irm "https://christitus.com/win" | iex}';
 
+#Refreshes the powershell path to use winget
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User");
+
 #installs a program that keeps the computer from sleeping and then sets it to keep awake for 15 mins
 winget install ZhornSoftware.Caffeine --source winget --force;
 
@@ -12,7 +15,7 @@ winget install ZhornSoftware.Caffeine --source winget --force;
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User");
 
 #Actives Caffeine, keeping the computer on for 15 mins while we work
-Caffeine -activefor:15 -replace;start-sleep 5;
+Caffeine -activefor:15 -replace;
 
 #Kicks inactive users from the computer to prevent people from remaining logged in and drawing resources from the current active user
 git clone https://github.com/23jrg/Kick-Inactive-Users;.\Kick-Inactive-Users\setup.bat;
