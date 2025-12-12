2b76417f361c52ff9d212014672f162c56091e2f
23jrg <jrg71442@yahoo.com>
2025-12-12 11:20:46 -0500

    Update Quantum-Cleanup.ps1

--- Quantum-Cleanup.ps1
+++ Quantum-Cleanup.ps1
@@ -1,3 +1,4 @@
-Remove-Item -Path "C:\23jrg" -Force -Recurse; winget uninstall git.git -force;schtasks.exe /delete /f /TN Quantum-Cleanup;
+Remove-Item -Path "C:\23jrg" -Force -Recurse; winget uninstall git.git --force;schtasks.exe /delete /f /TN Quantum-Cleanup;
 
 Remove-Item $PSCommandPath -Force
+
