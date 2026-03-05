e4449bf52f174e867ad99a314e1e2046731199d2
23jrg <jrg71442@yahoo.com>
2026-03-05 14:09:01 -0500

    Update secondary.ps1

--- secondary.ps1
+++ secondary.ps1
@@ -3,7 +3,7 @@ set-executionpolicy remotesigned;a;y;
 
 #Guibased Tools
 #invoke-expression 'cmd /c start powershell -Command {irm https://get.activated.win | iex}';
-invoke-expression 'cmd /c start powershell -Command {iex "& { $(irm https://christitus.com/win) } -Config C:\23jrg\Quantum-Impeller\winutilconfig.json"}';
+invoke-expression 'cmd /c start powershell -Command {iex "& { $(irm https://christitus.com/win) } -Config C:\23jrg\Quantum-Impeller\winutilconfig.txt"}';
 
 #Refreshes the powershell path to use winget
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User");
