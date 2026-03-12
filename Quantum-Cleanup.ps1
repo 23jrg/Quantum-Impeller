c13853e982ed9549499ef69e10ac2a90e8bdcf58
23jrg <jrg71442@yahoo.com>
2026-03-12 11:53:57 -0400

    Update Quantum-Cleanup.ps1

--- Quantum-Cleanup.ps1
+++ Quantum-Cleanup.ps1
@@ -1,4 +1,4 @@
-. C:\23jrg\Winutil\functions\public\Invoke-WPFFixesUpdate.ps1; Invoke-WPFFixesUpdate
+#. C:\23jrg\Winutil\functions\public\Invoke-WPFFixesUpdate.ps1; Invoke-WPFFixesUpdate
 
 . C:\23jrg\Winutil\functions\public\Invoke-WPFSystemRepair.ps1; Invoke-WPFSystemRepair
 
@@ -7,5 +7,6 @@ Install-Module PSWindowsUpdate -Force;Get-WindowsUpdate;Install-WindowsUpdate -A
 Remove-Item -Path "C:\23jrg" -Force -Recurse; winget uninstall git.git --accept-source-agreements --all --silent --force --nowarn;schtasks.exe /delete /f /TN Quantum-Cleanup;
 #Remove-MpPreference -ExclusionPath "C:\23jrg";
 Remove-Item $PSCommandPath -Force
+
 
 
