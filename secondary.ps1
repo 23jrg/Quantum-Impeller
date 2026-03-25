3c089640ab294a345e98d553d9761247adac751b
23jrg <jrg71442@yahoo.com>
2026-03-25 16:14:27 -0400

    Update secondary.ps1

--- secondary.ps1
+++ secondary.ps1
@@ -2,38 +2,38 @@
 set-executionpolicy remotesigned;a;y;
 
 #Guibased Tools
-#invoke-expression 'cmd /c start powershell -Command {irm https://get.activated.win | iex}';
 invoke-expression 'cmd /c start powershell -Command {irm "https://christitus.com/win" | iex}';
 
+#Makes a .txt with the ID of the runner who ran this script (this is used later for cleanup)
 invoke-expression 'cmd /c start powershell -Command {C:\23jrg\Quantum-Impeller\quser.bat}'
 
 #Refreshes the powershell path to use winget
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User");
 
-#installs a program that keeps the computer from sleeping
+#installs a program called Caffeine that keeps the computer from sleeping
 winget install ZhornSoftware.Caffeine --source winget --force;
 
+#Sets Caffeine to  keep the computer awake for 5 minutes every time the runner logs in
 $WshShell = New-Object -COMObject WScript.Shell
 $CaffeineShortcut = $WshShell.CreateShortcut("$Home\desktop\caffeine.lnk")
 $CaffeineShortcut.Arguments = "-activefor:15 -replace"
 $CaffeineShortcut.TargetPath = "$Home\AppData\Local\Microsoft\WinGet\Packages\ZhornSoftware.Caffeine_Microsoft.Winget.Source_8wekyb3d8bbwe\caffeine64.exe"
 $CaffeineShortcut.Save()
 
-#Refreshes the powershell path to use all the cool stuff we just added to it
+#Refreshes the powershell path again to use all the cool stuff we just added to it
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User");
 
-#Kicks inactive users from the computer to prevent people from remaining logged in and drawing resources from the current active user
+#Pulls down a program that kicks inactive users from the computer to prevent people from remaining logged in and drawing resources from the current active user
 git clone https://github.com/23jrg/Kick-Inactive-Users AppData\Local\Temp\KIU
 
+#Creates an installer for the program and moves it to the desktop
 $WshShell = New-Object -COMObject WScript.Shell
 $LoiaShortcut = $WshShell.CreateShortcut("$Home\desktop\Install_LogOffInactiveAccounts.lnk")
 $LoiaShortcut.TargetPath = "$Home\AppData\Local\Temp\KIU\setup.bat"
 $LoiaShortcut.Save()
 
-#Windows Activator, the first line makes an exclusion in defender for the working directory because microsoft doesn't like MAS
+#Pulls down an Office edition changer, this powerful tool automates the ability to switch one installed Office edition for another
 #powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "c:\23jrg";
-#git clone https://github.com/massgravel/Microsoft-Activation-Scripts;.\Microsoft-Activation-Scripts\MAS\All-In-One-Version-KL\MAS_AIO.cmd;
-#git clone https://github.com/massgravel/Microsoft-Activation-Scripts;.\Microsoft-Activation-Scripts\MAS\Separate-Files-Version\Change_Office_Edition.cmd;
 git clone https://github.com/massgravel/Microsoft-Activation-Scripts c:\23jrg\Activator;C:\23jrg\Activator\MAS\Separate-Files-Version\Change_Office_Edition.cmd;
 
 #Handy Windows updater gets placed on the desktop
