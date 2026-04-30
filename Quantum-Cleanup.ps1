5c9d5f8b6ad3e28fe7bac0d41d50e2ca1a7257ba
23jrg <jrg71442@yahoo.com>
2026-04-30 16:30:50 -0400

    Update Quantum-Cleanup.ps1

--- Quantum-Cleanup.ps1
+++ Quantum-Cleanup.ps1
@@ -6,9 +6,20 @@ Install-Module PSWindowsUpdate -Force;Get-WindowsUpdate -Install -AcceptAll -Ign
 
 winget uninstall git.git --accept-source-agreements --all --silent --force --nowarn;#schtasks.exe /delete /f /TN Quantum-Clipper;
 
+[System.IO.File]::WriteAllText(
+        'C:\23jrg\Quantum-Impeller\user_id.txt',
+        ([System.IO.File]::ReadAllText('C:\23jrg\Quantum-Impeller\user_id.txt') -replace '\s')
+    )
+
+
+$user_id =  get-content "C:\23jrg\Quantum-Impeller\user_id.txt"
+
 logoff (get-content "C:\23jrg\Quantum-Impeller\session_id.txt");
 
 $ErrorActionPreference = 'Stop'
+
+Remove-Item -Path "C:\Users\$user_id\desktop\tools" -Force -Recurse; 
+
 $PSNativeCommandUseErrorActionPreference = $true
 
 Remove-Item -Path "C:\23jrg" -Force -Recurse; 
