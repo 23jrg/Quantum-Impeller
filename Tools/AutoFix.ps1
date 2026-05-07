b7712273a1713088cf5717699f575060400c6e21
23jrg <jrg71442@yahoo.com>
2026-05-07 14:21:04 -0400

    Update and rename AutoFix.ps1 to AutoFix.bat

--- Tools/AutoFix.ps1
+++
@@ -1,6 +0,0 @@
-Caffeine -replace;
-    Start-Process cmd.exe -ArgumentList "/c chkdsk /scan /perf" -NoNewWindow -Wait
-    Start-Process cmd.exe -ArgumentList "/c sfc /scannow" -NoNewWindow -Wait
-    Start-Process cmd.exe -ArgumentList "/c dism /online /cleanup-image /restorehealth" -NoNewWindow -Wait
-Install-Module PSWindowsUpdate -Force;Get-WindowsUpdate -Install -AcceptAll -AutoReboot
-shutdown -r -t 1
