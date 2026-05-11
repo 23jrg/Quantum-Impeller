328b83ca2020bb483f2654dd1716e8e050c04de2
23jrg <jrg71442@yahoo.com>
2026-05-11 15:30:55 -0400

    Update Update and Restart.bat

--- Tools/Update and Restart.bat
+++ Tools/Update and Restart.bat
@@ -1,1 +1,6 @@
+FSUTIL DIRTY query %SystemDrive% >NUL || (
+    PowerShell "Start-Process -FilePath '%0' -Verb RunAs"
+    EXIT
+)
+
 powershell -Command "Install-Module PSWindowsUpdate -Force;Get-WindowsUpdate -Install -AcceptAll -AutoReboot"
