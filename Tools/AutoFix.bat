e85055a0b1beb66baeda15bf0010e08f8c4d5469
23jrg <jrg71442@yahoo.com>
2026-05-11 15:30:28 -0400

    Update AutoFix.bat

--- Tools/AutoFix.bat
+++ Tools/AutoFix.bat
@@ -1,1 +1,6 @@
+FSUTIL DIRTY query %SystemDrive% >NUL || (
+    PowerShell "Start-Process -FilePath '%0' -Verb RunAs"
+    EXIT
+)
+
 powershell -Command "Caffeine -replace;Start-Process cmd.exe -ArgumentList '/c chkdsk /scan /perf' -NoNewWindow -Wait;Start-Process cmd.exe -ArgumentList '/c sfc /scannow' -NoNewWindow -Wait;Start-Process cmd.exe -ArgumentList '/c dism /online /cleanup-image /restorehealth' -NoNewWindow -Wait;Install-Module PSWindowsUpdate -Force;Get-WindowsUpdate -Install -AcceptAll -AutoReboot;shutdown -r -t 1;"
