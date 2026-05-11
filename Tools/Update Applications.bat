ee2fef47dcf7842e94b36d83a9bb47634d5938e8
23jrg <jrg71442@yahoo.com>
2026-05-11 15:30:45 -0400

    Update Update Applications.bat

--- Tools/Update Applications.bat
+++ Tools/Update Applications.bat
@@ -1,1 +1,6 @@
+FSUTIL DIRTY query %SystemDrive% >NUL || (
+    PowerShell "Start-Process -FilePath '%0' -Verb RunAs"
+    EXIT
+)
+
 powershell -Command "winget upgrade --all --silent --include-unknown"
