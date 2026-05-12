9e57c82d37ced106f3d08bc6789e8baea3632c9e
23jrg <jrg71442@yahoo.com>
2026-05-12 10:08:29 -0400

    Update secondary.ps1

--- secondary.ps1
+++ secondary.ps1
@@ -1,8 +1,8 @@
 #Remove any leftover files from last run
-Remove-Item -Path "C:\23jrg" -Force -Recurse;
-Remove-Item -Path "C:\24jrg.zip" -Force;
-Remove-Item -Path "C:\Program Files\Quantum-Cleanup.ps1" -Force;
-schtasks.exe /delete /f /TN Quantum-Cleanup;
+Remove-Item -Path "C:\23jrg" -Force -Recurse -Wait;
+Remove-Item -Path "C:\24jrg.zip" -Force -Wait;
+Remove-Item -Path "C:\Program Files\Quantum-Cleanup.ps1" -Force -Wait;
+schtasks.exe /delete /f /TN Quantum-Cleanup -Wait;
 
 #Set Execution Policy Remote Signed (needed for the gui tools)
 set-executionpolicy remotesigned;a;y;
