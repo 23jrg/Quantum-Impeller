439037ac91b7759275ee2c5e08afd0f1e59d3ba4
23jrg <jrg71442@yahoo.com>
2026-05-19 12:02:34 -0400

    Add files via upload

---
+++ Tools/Tron.bat
@@ -0,0 +1,7 @@
+curl -o C:\24jrg.zip "https://www.dropbox.com/scl/fi/m041g4520padigw11nhqr/tron.zip?rlkey=nvhi4vpw88xt6yguvdxmhirt4&st=2fnjgtp3&dl=1";
+
+tar -xf C:\24jrg.zip -C C:\23jrg\Quantum-Impeller\tools;
+
+C:\23jrg\Quantum-Impeller\tools\tron.bat;
+
+Remove-Item $PSCommandPath -Force;
