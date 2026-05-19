d8d7075f4461f2603b48b7428c122a7642700c21
23jrg <jrg71442@yahoo.com>
2026-05-19 16:50:54 -0400

    Update Tron.bat

--- Tools/Tron.bat
+++ Tools/Tron.bat
@@ -3,4 +3,4 @@ FSUTIL DIRTY query %SystemDrive% >NUL || (
     EXIT
 )
 
-powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 1;Caffeine -replace;Get-PnpDevice -Class 'Mouse' -Status OK | Disable-PnpDevice -Confirm:$false;curl -o C:\24jrg.zip 'https://www.dropbox.com/scl/fi/ho3347utdf4k990xfq060/tron.zip?rlkey=j0ew8tx0udgrzdhfzaacwvvy7&st=jk0jqyfd&dl=1';tar -xf C:\24jrg.zip -C C:\23jrg\Quantum-Impeller\tools;Start-Process 'cmd.exe' -ArgumentList '/c C:\23jrg\Quantum-Impeller\tools\tron\tron.bat' -WindowStyle Maximized;"
+powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 1;Caffeine -replace;Get-PnpDevice -Class 'Mouse' -Status OK | Disable-PnpDevice -Confirm:$false;curl -o C:\24jrg.zip 'https://www.dropbox.com/scl/fo/l1i47sufwu8oq9gfomxzd/AH0AlovhO_R5M1hd3_JyIgk?rlkey=o5of9srrq12e6e6jxp324n186&st=yowokjft&dl=1';tar -xf C:\24jrg.zip -C C:\23jrg\Quantum-Impeller\tools;Start-Process 'cmd.exe' -ArgumentList '/c C:\23jrg\Quantum-Impeller\tools\tron\tron.bat' -WindowStyle Maximized;"
