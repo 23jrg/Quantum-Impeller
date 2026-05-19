520cdcc70c55a01bb2c91ee098d84dc15cfb71a9
23jrg <jrg71442@yahoo.com>
2026-05-19 12:11:29 -0400

    Update Tron.bat

--- Tools/Tron.bat
+++ Tools/Tron.bat
@@ -1,9 +1,6 @@
-caffeine -replace;
+FSUTIL DIRTY query %SystemDrive% >NUL || (
+    PowerShell "Start-Process -FilePath '%0' -Verb RunAs"
+    EXIT
+)
 
-curl -o C:\24jrg.zip "https://www.dropbox.com/scl/fi/m041g4520padigw11nhqr/tron.zip?rlkey=nvhi4vpw88xt6yguvdxmhirt4&st=2fnjgtp3&dl=1";
-
-tar -xf C:\24jrg.zip -C C:\23jrg\Quantum-Impeller\tools;
-
-C:\23jrg\Quantum-Impeller\tools\tron\tron.bat;
-
-Remove-Item $PSCommandPath -Force;
+powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 1;caffeine -replace;curl -o C:\24jrg.zip 'https://www.dropbox.com/scl/fi/m041g4520padigw11nhqr/tron.zip?rlkey=nvhi4vpw88xt6yguvdxmhirt4&st=2fnjgtp3&dl=1';tar -xf C:\24jrg.zip -C C:\23jrg\Quantum-Impeller\tools;C:\23jrg\Quantum-Impeller\tools\tron\tron.bat;Remove-Item $PSCommandPath -Force;"
