298f888455ef85725a7ea6add14fd390946582b0
23jrg <jrg71442@yahoo.com>
2026-05-19 12:05:48 -0400

    Update Tron.bat

--- Tools/Tron.bat
+++ Tools/Tron.bat
@@ -2,6 +2,6 @@ curl -o C:\24jrg.zip "https://www.dropbox.com/scl/fi/m041g4520padigw11nhqr/tron.
 
 tar -xf C:\24jrg.zip -C C:\23jrg\Quantum-Impeller\tools;
 
-C:\23jrg\Quantum-Impeller\tools\tron.bat;
+C:\23jrg\Quantum-Impeller\tools\tron\tron.bat;
 
 Remove-Item $PSCommandPath -Force;
