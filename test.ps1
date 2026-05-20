8600eacd9bc21856cf56b5f060cfced2ec2bdda7
23jrg <jrg71442@yahoo.com>
2026-05-20 13:30:37 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,13 +1,43 @@
-#Profile Customization start__________________________________________________________________________________________________________________________________
+#Remove any leftover files from last run
+Remove-Item -Path "C:\Program Files\Quantum-Cleanup.ps1" -Force;
+schtasks.exe /delete /f /TN Quantum-Cleanup;
+
+#Set Execution Policy Remote Signed (needed for the gui tools)
+set-executionpolicy remotesigned;a;y;
+
+powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "c:\23jrg";
+powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "c:\24jrg";
+#Automatic debloat then launches the Guibased Tools
+#git clone https://github.com/raphire/win11debloat c:\23jrg\win11debloat
+curl -o C:\24jrg.zip https://github.com/Raphire/Win11Debloat/archive/refs/heads/master.zip;
+tar -xf C:\24jrg.zip -C C:\23jrg\;
+ren C:\23jrg\Win11Debloat-master win11debloat;
+Start-Process powershell.exe -ArgumentList "-File", "C:\23jrg\Quantum-Impeller\Xephora-Threat-Remediation-Scripts\OneLaunch\OneLaunch-Remediation-Script.ps1"
+Start-Process powershell.exe -ArgumentList "-File", "C:\23jrg\Quantum-Impeller\Xephora-Threat-Remediation-Scripts\OneStart\OneStart-Remediation-Script.ps1"
+Start-Process powershell.exe -ArgumentList "-File", "C:\23jrg\Quantum-Impeller\Xephora-Threat-Remediation-Scripts\OneBrowser\OneBrowser-Remediation-Script.ps1"
+Start-Process powershell.exe -ArgumentList "-File", "C:\23jrg\Quantum-Impeller\Xephora-Threat-Remediation-Scripts\WaveBrowser\WaveBrowser-Remediation-Script-Win10-BrowserKill.ps1"
+Start-Process powershell.exe -ArgumentList "-File", "C:\23jrg\win11debloat\Win11Debloat.ps1", '-Silent', '-CreateRestorePoint', '-Config', "C:\23jrg\Quantum-Impeller\Win11Debloat-Config.json"
+Start-Process powershell.exe -ArgumentList "-File", "C:\23jrg\Quantum-Impeller\AI_Uninstaller.ps1", '-noninteractive', '-alloptions'
+Start-Process powershell.exe -ArgumentList "-File", "C:\23jrg\Quantum-Impeller\winutil.ps1"
+
+#invoke-expression 'cmd /c start powershell -Command {irm "https://christitus.com/win" | iex}';
+C:\23jrg\Quantum-Impeller\quser.bat
+#invoke-expression 'cmd /c start powershell -Command {C:\23jrg\Quantum-Impeller\quser.bat}';
+
+#Profile Customization
 if ($env:USERNAME -eq "jgraham" -or $env:USERNAME -eq "Administrator" -or $env:USERNAME -eq "CISTECH") {
 
 $Host.UI.RawUI.BackgroundColor = "Black"
 $Host.UI.RawUI.ForegroundColor = "DarkYellow"
+
+# 1. Define the online image URL and local save path
 $url = "https://images4.alphacoders.com/101/1014815.png"
 $localPath = "$env:USERPROFILE\Pictures\online_wallpaper.jpg"
 
+# 2. Download the image from the web
 Invoke-WebRequest -Uri $url -OutFile $localPath
 
+# Create the registry key if it does not exist
 if (!(Test-Path $cspPath)) {
     New-Item -Path $cspPath -Force | Out-Null
 }
@@ -22,29 +52,27 @@ public class Wallpaper {
 }
 "@
 
-Add the type and apply the wallpaper
+# 4. Add the type and apply the wallpaper
 Add-Type -TypeDefinition $code -Language CSharp
 [Wallpaper]::SystemParametersInfo(20, 0, $localPath, 3)
 
 $RegKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"
 
-#Set Apps to Dark
+# Set Apps to Dark
 Set-ItemProperty -Path $RegKeyPath -Name "AppsUseLightTheme" -Value 0 -Type Dword -Force
 
-#Set System to Dark
+# Set System to Dark
 Set-ItemProperty -Path $RegKeyPath -Name "SystemUsesLightTheme" -Value 0 -Type Dword -Force
 
-Write-Host "Dark Mode Set Successfully"
-
-#Define the Yellow accent color in hex (AABBGGRR format for registry)
+# Define the Yellow accent color in hex (AABBGGRR format for registry)
 $yellowHex = 0xFF009AC4
 
-#Update Registry for Personalization Colors
+# Update Registry for Personalization Colors
 $registryPath = "HKCU:\Software\Microsoft\Windows\DWM"
 Set-ItemProperty -Path $registryPath -Name "AccentColor" -Value $yellowHex
 Set-ItemProperty -Path $registryPath -Name "ColorPrevalence" -Value 1
 
-#Update User Personalization for the "Yellow" theme color
+# Update User Personalization for the "Yellow" theme color
 $personalizePath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Accent"
 Set-ItemProperty -Path $personalizePath -Name "AccentColorMenu" -Value $yellowHex
 
@@ -62,7 +90,7 @@ Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" 
 taskkill /f /im OneDrive.exe
 
 #Unpin everything from the taskbar
-$appsToUnpin = @("Microsoft Store", "Mail", "Calculator", "Microsoft Edge", "Copilot", "Microsoft 365 Copilot", "Outlook", "Outlook (New)")
+$appsToUnpin = @("Microsoft Store", "Mail", "Calculator", "Microsoft Edge", "Copilot", "Microsoft 365 Copilot", "Outlook")
 $shell = New-Object -Com Shell.Application
 $taskbarItems = $shell.NameSpace("shell:::{4234d49b-0245-4df3-b780-3893943456e1}").Items()
 foreach ($appname in $appsToUnpin) {
@@ -73,8 +101,152 @@ foreach ($appname in $appsToUnpin) {
     }
 }
 
-# Refresh the explorer process to apply changes without logging out
-Stop-Process -Name explorer -Force
-    
+#Center taskbar
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value 1
+
+#Refresh the explorer process to apply changes without logging out
+#Stop-Process -Name explorer -Force
+
 } 
-#Profile Customization end___________________________________________________________________________________________________________________________________
+
+#Set NTP pool time server
+#w32tm /config /syncfromflags:manual /manualpeerlist:"0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org
+
+#Set Timezone to EST
+C:\Windows\System32\tzutil.exe /s "Eastern Standard Time"
+
+#Sync Clock
+w32tm /resync /force
+Restart-Service w32time
+
+#Refreshes the powershell path to use winget
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User");
+
+#installs a program that keeps the computer from sleeping and then sets it to keep awake for 15 mins
+winget install ZhornSoftware.Caffeine --source winget --force;
+
+#installs a tiling manager
+#winget install GlazeWM --source winget --force;
+
+#Refreshes the powershell path to use all the cool stuff we just added to it
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User");
+
+#Actives Caffeine, keeping the computer on for 15 mins while we work, sets computer to remain on for 15 mins after we sign in
+if (-not (Test-Path -Path "c:\users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\caffeine_wrk_hrs.lnk")) {
+
+    Caffeine -activefor:15 -replace;
+
+$WshShell = New-Object -COMObject WScript.Shell
+$CaffeineShortcut = $WshShell.CreateShortcut("$Home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\caffeine.lnk")
+$CaffeineShortcut.Arguments = "-activefor:15 -replace"
+$CaffeineShortcut.TargetPath = "$Home\AppData\Local\Microsoft\WinGet\Packages\ZhornSoftware.Caffeine_Microsoft.Winget.Source_8wekyb3d8bbwe\caffeine64.exe"
+$CaffeineShortcut.Save()
+
+}
+
+#Kicks inactive users from the computer to prevent people from remaining logged in and drawing resources from the current active user
+#git  https://github.com/23jrg/Kick-Inactive-Users;.\Kick-Inactive-Users\setup.bat;
+#git  https://github.com/23jrg/Kick-Inactive-Users C:\LogInactiveOff;
+curl -o C:\24jrg.zip https://github.com/23jrg/Kick-Inactive-Users/archive/refs/heads/main.zip;
+tar -xf C:\24jrg.zip -C C:\
+ren c:\Kick-Inactive-Users-main LogInactiveOff
+
+schtasks.exe /Create /XML 'C:\LogInactiveOff\Log off inactive users.xml' /tn LogInactiveOff;
+
+#Sets a copy of the Toolkit on the user's desktop
+$WshShell = New-Object -COMObject WScript.Shell
+$ToolShortcut = $WshShell.CreateShortcut("$Home\desktop\TechTools.lnk")
+$ToolShortcut.TargetPath = "C:\23jrg\Quantum-Impeller\Tools"
+$ToolShortcut.IconLocation = "C:\23jrg\Quantum-Impeller\favicon.ico"
+$ToolShortcut.Save()
+
+#Puts a shortcut of Caffeine in the tech tools, set to stay active from 9:30-6:00
+$WshShell = New-Object -COMObject WScript.Shell
+$Caffeine_wrk_hrs = $WshShell.CreateShortcut("C:\23jrg\Quantum-Impeller\Tools\caffeine_wrk_hrs.lnk")
+$Caffeine_wrk_hrs.Arguments = " -activeperiods:0930-1800 -replace"
+$Caffeine_wrk_hrs.TargetPath = "$Home\AppData\Local\Microsoft\WinGet\Packages\ZhornSoftware.Caffeine_Microsoft.Winget.Source_8wekyb3d8bbwe\caffeine64.exe"
+$Caffeine_wrk_hrs.Save()
+
+#Creates shortcut to startup folder
+$WshShell = New-Object -COMObject WScript.Shell
+$Startup = $WshShell.CreateShortcut("C:\23jrg\Quantum-Impeller\Tools\startup.lnk")
+$Startup.TargetPath = "$Home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+$Startup.Save()
+
+#Copies the Inactive User Uninstaller to the tools folder
+Copy-Item -Path "C:\LogInactiveOff\uninstall.bat" -Destination "C:\23jrg\Quantum-Impeller\Tools"
+Rename-Item -Path "C:\23jrg\Quantum-Impeller\Tools\uninstall.bat" -NewName "Uninstall_Log_Inactive_Off.bat"
+
+#Pulls down an Office edition changer, this powerful tool automates the ability to switch one installed Office edition for another
+#git clone https://github.com/massgravel/Microsoft-Activation-Scripts c:\23jrg\Activator;
+curl -o C:\24jrg.zip https://github.com/massgravel/Microsoft-Activation-Scripts/archive/refs/heads/master.zip;
+tar -xf C:\24jrg.zip -C C:\23jrg;
+ren C:\23jrg\Microsoft-Activation-Scripts-master Activator;
+C:\23jrg\Activator\MAS\Separate-Files-Version\Change_Office_Edition.cmd;
+
+$WshShell = New-Object -COMObject WScript.Shell
+$Shortcut4 = $WshShell.CreateShortcut("C:\23jrg\Quantum-Impeller\Tools\Activator.lnk")
+$Shortcut4.TargetPath = "C:\23jrg\Activator\MAS\All-In-One-Version-KL\MAS_AIO.cmd"
+$Shortcut4.Save()
+
+
+#Handy Windows updater gets placed in the tools folder
+#git clone https://github.com/23jrg/MediaCreationTool.bat c:\23jrg\MediaCreationTool.bat;
+curl -o C:\24jrg.zip https://github.com/23jrg/MediaCreationTool.bat/archive/refs/heads/main.zip;
+tar -xf C:\24jrg.zip -C C:\23jrg;
+ren c:\23jrg\MediaCreationTool.bat-main "MediaCreationTool.bat"
+
+$WshShell = New-Object -COMObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\23jrg\Quantum-Impeller\Tools\11_Upgrade_Tool.lnk")
+$Shortcut.TargetPath = "C:\23jrg\MediaCreationTool.bat\MediaCreationTool.bat"
+$Shortcut.Save()
+
+#Anti-AI
+#git clone https://github.com/zoicware/RemoveWindowsAI c:\23jrg\RemoveAI
+curl -o C:\24jrg.zip https://github.com/zoicware/RemoveWindowsAI/archive/refs/heads/main.zip;
+tar -xf C:\24jrg.zip -C C:\23jrg;
+ren C:\23jrg\RemoveWindowsAI-main RemoveAI
+
+$WshShell = New-Object -COMObject WScript.Shell
+$Shortcut3 = $WshShell.CreateShortcut("C:\23jrg\Quantum-Impeller\Tools\Remove-AI.lnk")
+$Shortcut3.TargetPath = "powershell.exe"
+$Shortcut3.Arguments = [string]"C:\23jrg\RemoveAI\RemoveWindowsAi.ps1 -noninteractive -alloptions"
+$Shortcut3.Save()
+
+#Deployment Emailer gets placed in the Tools folder
+Copy-Item -Path "\\ve-fsvr\CIS_Internal_Data\Tools\DeploymentEmails\SendEmail.bat" -Destination "C:\23jrg\"
+Copy-Item -Path "\\ve-fsvr\CIS_Internal_Data\Tools\DeploymentEmails\Send-Gmail-Auto.ps1" -Destination "C:\23jrg\"
+
+
+$WshShell = New-Object -COMObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\23jrg\Quantum-Impeller\Tools\Send_Deployment_Email.lnk")
+$Shortcut.TargetPath = "C:\23jrg\SendEmail.bat"
+$Shortcut.Save()
+
+#Set Quick Machine Recovery on 24h2+ computers
+reagentc.exe /setrecoverysettings /path C:\23jrg\Quantum-Impeller\qmr_settings.xml;
+
+#Set Page File to automatically managed
+Set-CimInstance -Query "SELECT * FROM Win32_ComputerSystem" -Property @{AutomaticManagedPagefile=$True}
+
+#Set fast startup to disabled
+reg add 'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power' -v 'HiberbootEnabled' /t REG_DWORD -d 0 /f
+
+#Disable location popups
+reg add 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location' -v 'ShowGlobalPrompts' /t REG_DWORD -d 0 /f
+
+#Disable Resume
+taskkill /IM CrossDeviceResume.exe
+reg add "HKLM\SOFTWARE\Microsoft\PolicyManager\default\Connectivity\DisableCrossDeviceResume" /v "value" /t REG_DWORD /d "1" /f
+Get-AppxPackage *Microsoft.CrossDeviceExperienceHost* | Remove-AppxPackage
+
+#Grabs some functions to be run on cleanup
+#git clone https://github.com/ChrisTitusTech/winutil c:\23jrg\Winutil
+#curl -o C:\24jrg.zip https://github.com/ChrisTitusTech/winutil/archive/refs/heads/main.zip;
+#tar -xf C:\24jrg.zip -C C:\23jrg;
+#ren C:\23jrg\winutil-main Winutil
+
+#Cleans up leftovers on next startup
+schtasks.exe /Create /XML 'C:\23jrg\Quantum-Impeller\Quantum-Cleanup.xml' /tn Quantum-Cleanup;
+#schtasks.exe /Create /XML 'C:\23jrg\Quantum-Impeller\Quantum-Clipper.xml' /tn Quantum-Clipper;
+Move-Item -Path "C:\23jrg\Quantum-Impeller\Quantum-Cleanup.ps1" -Destination "C:\Program Files\"
