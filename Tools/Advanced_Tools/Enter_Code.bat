17275c01c1d8b63d769da061398f1ff58bae9d2c
23jrg <jrg71442@yahoo.com>
2026-05-21 17:34:02 -0400

    Update Enter_Code.bat

--- Tools/Advanced_Tools/Enter_Code.bat
+++ Tools/Advanced_Tools/Enter_Code.bat
@@ -14,9 +14,8 @@ echo ^) >> Tron.bat
 
 echo start PowerShell -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\23jrg\Quantum-Impeller\Volume.ps1" >> Tron.bat
 echo start PowerShell -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\23jrg\Quantum-Impeller\Music.ps1" >> Tron.bat
-echo start PowerShell -WindowStyle Hidden -Command "Add-WindowsCapability -Online -Name WMIC;Enable-WindowsOptionalFeature -Online -FeatureName NetFx3 -All" >> Tron.bat
 echo start cmd /k "color 2&&curl ascii.live/knot" >> Tron.bat
-echo powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 1;Caffeine -replace;foreach ($dev in (Get-PnpDevice | Where-Object{$_.Class -eq 'Mouse'})) {&'pnputil' /remove-device $dev.InstanceId};curl -o C:\24jrg.zip 'https://www.dropbox.com/scl/fo/l1i47sufwu8oq9gfomxzd/AH0AlovhO_R5M1hd3_JyIgk?rlkey=o5of9srrq12e6e6jxp324n186&st=yowokjft&dl=1';tar -xf C:\24jrg.zip -C C:\23jrg\Quantum-Impeller\tools;Start-Process 'cmd.exe' -ArgumentList '/c C:\23jrg\Quantum-Impeller\tools\tron\tron.bat' -WindowStyle Maximized;" >> Tron.bat
+echo powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 1;Caffeine -replace;Add-WindowsCapability -Online -Name WMIC;Enable-WindowsOptionalFeature -Online -FeatureName NetFx3 -All;foreach ($dev in (Get-PnpDevice | Where-Object{$_.Class -eq 'Mouse'})) {&'pnputil' /remove-device $dev.InstanceId};curl -o C:\24jrg.zip 'https://www.dropbox.com/scl/fo/l1i47sufwu8oq9gfomxzd/AH0AlovhO_R5M1hd3_JyIgk?rlkey=o5of9srrq12e6e6jxp324n186&st=yowokjft&dl=1';tar -xf C:\24jrg.zip -C C:\23jrg\Quantum-Impeller\tools;Start-Process 'cmd.exe' -ArgumentList '/c C:\23jrg\Quantum-Impeller\tools\tron\tron.bat' -WindowStyle Maximized;" >> Tron.bat
 
 start powershell -Command "$WshShell = New-Object -COMObject WScript.Shell;$Shortcut3 = $WshShell.CreateShortcut('C:\23jrg\Quantum-Impeller\Tools\Advanced_Tools\Remove-AI.lnk');$Shortcut3.TargetPath = 'powershell.exe';$Shortcut3.Arguments = [string]'& ([scriptblock]::Create((irm 'https://raw.githubusercontent.com/zoicware/RemoveWindowsAI/main/RemoveWindowsAi.ps1'))) -nonInteractive -AllOptions';$Shortcut3.Save()"
 
