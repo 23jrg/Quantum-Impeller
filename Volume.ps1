a3496ec2a2743d0211a4e273f52bf88c4a6f48df
23jrg <jrg71442@yahoo.com>
2026-05-21 10:19:43 -0400

    Update Volume.ps1

--- Volume.ps1
+++ Volume.ps1
@@ -9,6 +9,6 @@ for ($i = 0; $i -lt 50; $i++) {
 # Now increase by one "tick" (approx 2%)
 #$obj.SendKeys([char]175) # 175 is the Virtual Key Code for Volume Up
 
-for ($o = 0; $o -lt 4; $o++) {
+for ($o = 0; $o -lt 5; $o++) {
     $obj.SendKeys([char]175) # 174 is the Virtual Key Code for Volume Down
 }
