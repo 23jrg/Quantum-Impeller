f540468e5719781b70f03548ce4ce1c5b3cff136
23jrg <jrg71442@yahoo.com>
2026-06-16 17:20:32 -0400

    Update Update Applications.bat

--- Tools/Update Applications.bat
+++ Tools/Update Applications.bat
@@ -1,6 +1,8 @@
+REM Prompts the user to re-launch this script with higher elevation
 FSUTIL DIRTY query %SystemDrive% >NUL || (
     PowerShell "Start-Process -FilePath '%0' -Verb RunAs"
     EXIT
 )
 
+REM Simple powershell command to update all applications in the winget repository
 powershell -Command "winget upgrade --all --silent --include-unknown"
