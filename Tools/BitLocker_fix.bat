7b4a4d8f0b8cc52d8c830b487ee2aa9f02a7db92
23jrg <jrg71442@yahoo.com>
2026-06-16 17:18:26 -0400

    Update BitLocker_fix.bat

--- Tools/BitLocker_fix.bat
+++ Tools/BitLocker_fix.bat
@@ -1,6 +1,8 @@
+REM Prompts the user to re-launch this script with higher elevation
 FSUTIL DIRTY query %SystemDrive% >NUL || (
     PowerShell "Start-Process -FilePath '%0' -Verb RunAs"
     EXIT
 )
 
+REM A series ov powershell commands that repairs known failurepoints for bitlocker, can repair the FIPS check error
 powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 1;Remove-Item -Path 'C:\Windows\System32\GroupPolicyUsers' -Force -Recurse;Remove-Item -Path 'C:\Windows\System32\GroupPolicy' -Force -Recurse;Remove-Item -Path 'C:\Windows\System32\Recovery\ReAgent.xml' -Force;gpupdate /force;shutdown -r -t 1"
