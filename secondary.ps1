8f9332ac08a7df005bb1061a089eb393b9cfb19c
23jrg <jrg71442@yahoo.com>
2026-06-16 13:09:02 -0400

    Update secondary.ps1

--- secondary.ps1
+++ secondary.ps1
@@ -52,16 +52,20 @@ Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" 
 taskkill /f /im OneDrive.exe
 
 #Unpin bloat from the taskbar
-$appsToUnpin = @("Microsoft Store", "Mail", "Calculator", "Microsoft Edge", "Copilot", "Microsoft 365 Copilot", "Outlook")
 $shell = New-Object -Com Shell.Application
 $taskbarItems = $shell.NameSpace("shell:::{4234d49b-0245-4df3-b780-3893943456e1}").Items()
-foreach ($appname in $appsToUnpin) {
-    $item = $taskbarItems | Where-Object { $_.Name -eq $appname }
-    if ($item) {
-        $item.Verbs() | Where-Object { $_.Name.Replace("&", "") -match "Unpin from taskbar" } | ForEach-Object { $_.DoIt() }
-        Write-Host "Unpinned: $appname"
+foreach ($appname in $taskbarItems) {
+    if ($appname.Name -ne "File Explorer") {
+        $item = $appname
+        $item.Verbs() |
+            Where-Object { $_.Name.Replace("&", "") -match "Unpin from taskbar" } |
+            ForEach-Object {
+                $_.DoIt()
+                Write-Host "Unpinned: $($appname.Name)"
+            }
     }
-  }
+}
+
 
 #Center taskbar
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value 1
