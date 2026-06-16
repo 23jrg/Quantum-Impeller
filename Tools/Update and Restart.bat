ceff81d9eabedeff75ac00543f7a46efa30a87d0
23jrg <jrg71442@yahoo.com>
2026-06-16 17:21:27 -0400

    Update Update and Restart.bat

--- Tools/Update and Restart.bat
+++ Tools/Update and Restart.bat
@@ -1,6 +1,8 @@
+REM Prompts the user to re-launch this script with higher elevation
 FSUTIL DIRTY query %SystemDrive% >NUL || (
     PowerShell "Start-Process -FilePath '%0' -Verb RunAs"
     EXIT
 )
 
-powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 1;Install-Module PSWindowsUpdate -Force;Get-WindowsUpdate -Install -AcceptAll -AutoReboot"
+REM Powershell command to suspend bitlocker, then install updates and reboot
+powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 1;Install-Module PSWindowsUpdate -Force;Get-WindowsUpdate -Install -AcceptAll -AutoReboot;shutdown -r -t 1"
