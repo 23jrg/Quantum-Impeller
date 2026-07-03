ea2962aee5816ceac41b10bf9c87bd0f5d791677
23jrg <jrg71442@yahoo.com>
2026-07-03 12:36:09 -0400

    Update AutoFix and Restart.bat

--- Tools/AutoFix and Restart.bat
+++ Tools/AutoFix and Restart.bat
@@ -4,17 +4,16 @@ FSUTIL DIRTY query %SystemDrive% >NUL || (
     EXIT
 )
 
-REM Queues up a windows reinstall on 24H2+ computers
-start powershell -file "C:\23jrg\Quantum-Impeller\InplaceReinstall.ps1"
-
-TIMEOUT /T 30
-
-start powershell -Command "(New-Object -ComObject Shell.Application).MinimizeAll()"
-
-TIMEOUT /T 10
-
 REM Launches a script in the backround that locks the computer if someone moves the mouse, useful for running the following repair commands onsite without having to worry about users messing with the computer while you're logged in
 start powershell -file "C:\23jrg\Quantum-Impeller\tools\Lock_on_mouse_movement.ps1"
 
 REM A series of powershell commands that suspends bitlocker, keeps the screen awake, disables the mouse until reboot (or reseat) then launches chkdsk, sfc, dism, Windows Update, and finally a reboot
-start /max powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 3;Caffeine -replace;foreach ($dev in (Get-PnpDevice | Where-Object{$_.Class -eq 'Mouse'})) {&'pnputil' /remove-device $dev.InstanceId};Start-Process cmd.exe -ArgumentList '/c chkdsk /scan /perf' -NoNewWindow -Wait;Start-Process cmd.exe -ArgumentList '/c sfc /scannow' -NoNewWindow -Wait;Start-Process cmd.exe -ArgumentList '/c dism /online /cleanup-image /restorehealth' -NoNewWindow -Wait;$RepairPack = Get-TroubleshootingPack -Path C:\Windows\diagnostics\system\WindowsUpdate;Invoke-TroubleshootingPack -Pack $RepairPack -Unattended;Install-Module PSWindowsUpdate -Force;Import-Module PSWindowsUpdate;Get-WindowsUpdate -Install -AcceptAll -AutoReboot;shutdown -r -t 1"
+start /max powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 3;Caffeine -replace;foreach ($dev in (Get-PnpDevice | Where-Object{$_.Class -eq 'Mouse'})) {&'pnputil' /remove-device $dev.InstanceId};Start-Process cmd.exe -ArgumentList '/c chkdsk /scan /perf' -NoNewWindow -Wait;Start-Process cmd.exe -ArgumentList '/c sfc /scannow' -NoNewWindow -Wait;Start-Process cmd.exe -ArgumentList '/c dism /online /cleanup-image /restorehealth' -NoNewWindow -Wait;$RepairPack = Get-TroubleshootingPack -Path C:\Windows\diagnostics\system\WindowsUpdate;Invoke-TroubleshootingPack -Pack $RepairPack -Unattended;Install-Module PSWindowsUpdate -Force;Import-Module PSWindowsUpdate;Get-WindowsUpdate -Install -AcceptAll -AutoReboot"
+
+TIMEOUT /T 15
+
+start powershell -file "C:\23jrg\Quantum-Impeller\InplaceReinstall.ps1"
+
+TIMEOUT /T 30
+
+start powershell -Command "shutdown -r -t 1"
