7a6720560da6eb4789d6b55d5d638b3f52651dcb
23jrg <jrg71442@yahoo.com>
2026-07-06 17:06:10 -0400

    Update Update and Restart.bat

--- Tools/Update and Restart.bat
+++ Tools/Update and Restart.bat
@@ -4,5 +4,8 @@ FSUTIL DIRTY query %SystemDrive% >NUL || (
     EXIT
 )
 
+REM Change Windows registry keys for a more aggressive update
+start /wait powershell -Command "Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" -Name "AllowMUUpdateService" -Value 1;Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" -Name "RestartNotificationsAllowed2" -Value 1;Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings" -Name "IsExpedited" -Value 1;Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "NoAutoUpdate" -Value 0;exit"
+
 REM Powershell command to suspend bitlocker, then install updates and reboot
 powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 1;Install-Module PSWindowsUpdate -Force;Get-WindowsUpdate -Install -AcceptAll -AutoReboot;shutdown -r -t 1"
