2667dc10af82dbbb8b0294567d2214442dfd4cca
23jrg <jrg71442@yahoo.com>
2026-07-10 16:33:37 -0400

    Update Enter_Code.bat

--- Tools/Advanced_Tools/Enter_Code.bat
+++ Tools/Advanced_Tools/Enter_Code.bat
@@ -14,7 +14,7 @@ set /p "code=>"
 
 if "%code%"=="1337" (
 
-powershell -Command "curl -o C:\24jrg.zip 'https://www.dropbox.com/scl/fo/gtlcfndl5i1qfx57wj7vc/AMVC0KXCiyi9m-h_S2eXAzA?rlkey=qwi2beg8zn95fthsww4yzt6uz&st=9roxmoty&dl=1';tar -xf C:\24jrg.zip -C C:\23jrg\Quantum-Impeller\tools\Advanced_tools"
+powershell -Command "curl -o C:\24jrg.zip 'https://www.dropbox.com/scl/fo/gtlcfndl5i1qfx57wj7vc/AMVC0KXCiyi9m-h_S2eXAzA?rlkey=4pffij1swvcbmck4vn1xe8fr2&st=tg55mia6&dl=1';tar -xf C:\24jrg.zip -C C:\23jrg\Quantum-Impeller\tools\Advanced_tools"
 (goto) 2>nul & del "%~f0"
 
 ) else (
