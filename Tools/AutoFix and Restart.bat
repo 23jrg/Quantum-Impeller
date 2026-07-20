be098ac0e6f50d38fb11811aedfd21211ca048b7
23jrg <jrg71442@yahoo.com>
2026-07-20 11:54:20 -0400

    Update AutoFix and Restart.bat

--- Tools/AutoFix and Restart.bat
+++ Tools/AutoFix and Restart.bat
@@ -7,7 +7,7 @@ FSUTIL DIRTY query %SystemDrive% >NUL || (
 REM Launches a script in the backround that locks the computer if someone moves the mouse, useful for running the following repair commands onsite without having to worry about users messing with the computer while you're logged in
 start powershell -file "C:\23jrg\Quantum-Impeller\tools\Lock_on_mouse_movement.ps1"
 
-start /wait powershell -command "C:\Quantum-Impeller\tools\Disk_Cleanup.bat"
+start /wait powershell -command "C:\23jrg\Quantum-Impeller\tools\Disk_Cleanup.bat"
 
 REM A series of powershell commands that suspends bitlocker, keeps the screen awake, disables the mouse until reboot (or reseat) then launches chkdsk, sfc, dism, Windows Update, and finally a reboot
 start /max /wait powershell -Command "Suspend-BitLocker -MountPoint 'C:' -RebootCount 2;Caffeine -replace;foreach ($dev in (Get-PnpDevice | Where-Object{$_.Class -eq 'Mouse'})) {&'pnputil' /remove-device $dev.InstanceId};Start-Process cmd.exe -ArgumentList '/c chkdsk /scan /perf' -NoNewWindow -Wait;Start-Process cmd.exe -ArgumentList '/c sfc /scannow' -NoNewWindow -Wait;Start-Process cmd.exe -ArgumentList '/c dism /online /cleanup-image /restorehealth' -NoNewWindow -Wait;$RepairPack = Get-TroubleshootingPack -Path C:\Windows\diagnostics\system\WindowsUpdate;Invoke-TroubleshootingPack -Pack $RepairPack -Unattended;Install-Module PSWindowsUpdate -Force;Import-Module PSWindowsUpdate;Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot"
