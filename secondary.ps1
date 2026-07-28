e5bb9a680488c990f0f505e4c591796f91cb6588
23jrg <jrg71442@yahoo.com>
2026-07-28 12:33:56 -0400

    Update secondary.ps1

--- secondary.ps1
+++ secondary.ps1
@@ -19,8 +19,8 @@ if (-not $isAdmin) {
 Remove-Item -Path "C:\Program Files\Quantum-Cleanup.ps1" -Force;
 schtasks.exe /delete /f /TN Quantum-Cleanup;
 
-#Set Execution Policy Remote Signed (needed for the gui tools)
-set-executionpolicy remotesigned;a;y;
+# Set Execution Policy Remote Signed
+set-executionpolicy remotesigned -Scope Process -Force
 
 #Create an exclusion to prevent false positives
 powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath "c:\23jrg";
