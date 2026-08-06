aaa9d4febbbc663c5aa2aac0532457acf6a2e221
23jrg <jrg71442@yahoo.com>
2026-08-06 17:52:11 -0400

    Update SyncClock.bat

--- Tools/SyncClock.bat
+++ Tools/SyncClock.bat
@@ -31,5 +31,3 @@ FSUTIL DIRTY query %SystemDrive% >NUL || (
 tzutil.exe /s "Eastern Standard Time"
 
 w32tm /config /syncfromflags:manual /manualpeerlist:"0.pool.ntp.org 1.pool.ntp.org 2.pool.ntp.org 3.pool.ntp.org"&&w32tm /resync
-
-pause
